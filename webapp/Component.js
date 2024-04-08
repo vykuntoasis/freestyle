d9c7780052eece75be5f3223d62ad66c0a8eea45
vykuntarao tangudu <vykuntoasis@gmail.com>
2024-04-08 12:35:17 +0000

    freestyleappwithcappodestinationservice

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.anubhav.freestyle.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
